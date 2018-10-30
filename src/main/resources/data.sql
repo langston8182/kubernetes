75cadf51c233b06e296f44dd54afc4c3666b0cbf
Cyril Marchive <cyril.marchive@gmail.com>
2018-10-29 20:31:07 -0400

    Docker et mariadb

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+delete from PERSONNE;
+
+insert into PERSONNE(personne_id, date_naissance, nom, prenom) values('1', '1982-01-08', 'Marchive', 'Cyril');
--- src/test/java/com/cmarchive/kubernetes/integration/IntegrationTest.java
+++ src/test/java/com/cmarchive/kubernetes/integration/IntegrationTest.java
@@ -16,6 +16,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
@@ -34,6 +35,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @WebAppConfiguration
 @Sql("/data-test.sql")
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@Profile("dev")
 public class IntegrationTest {
 
     @Autowired
